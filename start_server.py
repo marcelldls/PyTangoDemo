5cb32c7bb2d22a998471c0b5b905f392e44d7501
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-06-28 13:02:32 +0200

    linting

--- start_server.py
+++ start_server.py
@@ -1,22 +1,22 @@
-'''Command line tool to start a Tango Device Server'''
+"""Command line tool to start a Tango Device Server"""
 
 import argparse
 import os
 
 parser = argparse.ArgumentParser(
-                            description='Starts a Tango Device Server'
+                            description="Starts a Tango Device Server"
                             )
-parser.add_argument('--nodb',
-                help='Run device server without a database',
-                action='store_true'
+parser.add_argument("--nodb",
+                help="Run device server without a database",
+                action="store_true"
                 )
 args = parser.parse_args()
 
 # Start device server: python <Server_file>.py <instance name>
-COMMAND = 'python tango_classes/MyTestDevice.py test'
+COMMAND = "python tango_classes/PogoTestDevice.py test"
 if args.nodb:
-    print('Start device server without database')
-    os.system(COMMAND + ' --nodb --dlist nodb/TestDevice/0 --port 10000')
+    print("Start device server without database")
+    os.system(COMMAND + " --nodb --dlist nodb/test_device/0 --port 10000")
 elif ~args.nodb:
-    print('Start device server')
+    print("Start device server")
     os.system(COMMAND)
