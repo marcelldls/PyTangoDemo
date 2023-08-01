6d0d7172cda6e6fac3fb0714a43239acd020fee4
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-01 10:48:12 +0100

    Start programmatic device building

---
+++ build_device.py
@@ -0,0 +1,42 @@
+import pickle 
+from src.builder import device_class_builder
+from src.util import spin
+
+attributes = {
+    "remember_bool": {
+        "attribute_type": "Read/Write",
+        "data_type": "Bool",
+        "data_format": "Scalar",
+        },
+    "remember_float": {
+        "attribute_type": "Read/Write",
+        "data_type": "Float",
+        "data_format": "Scalar",
+        },
+}
+
+commands = {
+    "spin": spin,
+}
+
+properties = {
+}
+
+dev_config = {
+    "attributes": attributes,
+    "commands": commands,
+    "properties": properties,
+}
+
+my_dev_class = device_class_builder(**dev_config)
+
+with open('tango_classes/BuildTestDevice.obj', 'wb') as filehandler:
+    pickle.dump(my_dev_class, filehandler)
+    print("Sucessfully stored device class")
+
+#####  delete below when before merge ##### 
+
+with open("tango_classes/BuildTestDevice.obj", 'rb') as filehandler:
+    device_class = pickle.load(filehandler)
+
+device_class.run_server(['test', '--nodb', '--dlist', 'nodb/test_device/0', '--port', '8888'])
