dd2bc3d69ebf17043d0002c356e3d2bc0f1a642e
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-01 14:40:43 +0100

    Successfully add attributes

--- build_device.py
+++ build_device.py
@@ -30,6 +30,18 @@ dev_config = {
 
 my_dev_class = device_class_builder(**dev_config)
 
+
+# Solve -> AttributeError: Can't pickle local object 'add_attributes.<locals>.dynamic_class'
+"""
 with open('tango_classes/BuildTestDevice.obj', 'wb') as filehandler:
     pickle.dump(my_dev_class, filehandler)
     print("Sucessfully stored device class")
+"""
+
+#####  delete everythinh below before merge #####
+"""
+with open("tango_classes/BuildTestDevice.obj", 'rb') as filehandler:
+    device_class = pickle.load(filehandler)
+"""
+
+my_dev_class.run_server(['test', '--nodb', '--dlist', 'nodb/test_device/0', '--port', '8888'])
--- src/builder.py
+++ src/builder.py
@@ -1,34 +1,34 @@
-from tango.server import Device
+from tango.server import Device, attribute, command
+from src.util import spin, dummy_measure_func
 
 
 def device_class_builder(attributes=None,
                          commands=None,
                          properties=None):
 
-    print("Create device class")
-    device_class = Device
-    add_attributes(device_class, attributes)
-    add_commands(device_class, attributes)
-    add_properties(device_class, attributes)
+    print("Building device class...")
+    device_class = add_attributes(Device, attributes)
+    device_class = add_commands(device_class, commands)
+    device_class = add_properties(device_class, properties)
 
     return device_class
 
 
 def add_attributes(device, attributes):
-    pass
 
+    class dynamic_class(device):
+        dummy_measure = attribute(dummy_measure_func, dtype=float)
 
-"""
-setattr(Foo, 'print_v', my_new_method)
-    @attribute(dtype=float)
-    def voltage(self):
-        return 1.23
-"""
+    print("Added attributes")
+
+    return dynamic_class
 
 
 def add_commands(device, commands):
-    pass
+    print("Added commands")
+    return device
 
 
 def add_properties(device, properties):
-    pass
+    print("Added properties")
+    return device
