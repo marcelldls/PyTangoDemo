cf8fa703fcb4296b68bf302910d083dd38bdff33
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-02 16:10:44 +0100

    Work on connections

--- run_client.py
+++ run_client.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 from builtins import open
 import argparse
 import json
-import socket
 import tango
 
 # Process command line arguments
@@ -28,27 +27,29 @@ args = parser.parse_args()
 # Process config file
 with open("config/config.json", "r", encoding="utf-8") as config_file:
     config = json.load(config_file)
-    cl_path = config["device_class_path"]
+    host = config["host"]
+    port = config["port"]
     dsr_name = config["device_server_name"]
     dev_name = config["device_name"]
+    cl_path = config["device_class_path"]
+    cl_type = config["device_class_type"]
+
+srvr_addr = "tango://"+host+":"+port+"/"
 
 if args.nodb:
-    hostname = socket.gethostname() + ":8888/"
-    nodb_name = "nodb/" + dev_name[dev_name.find("/") + 1:]
-    dev_name = hostname + nodb_name + "#dbase=no"
+    dev_name = srvr_addr + dev_name + "#dbase=no"
 
 elif args.test:
-    ADDRESS = "tango://127.0.0.1:8888"
     class_name = cl_path[cl_path.find("/") + 1: -3]
-    test_name = "/test/nodb/" + class_name
-    dev_name = ADDRESS + test_name + "#dbase=no"
-    print(dev_name)
+    dev_name = "test/nodb/" + class_name.lower()
+    dev_name = srvr_addr + dev_name + "#dbase=no"
 
 # Use PyTango
 print("Tango version:", tango.__version__)
 print("TANGO Database address:", tango.ApiUtil.get_env_var("TANGO_HOST"))
 
 # Do client things
+print("Connect to:", dev_name)
 test_device = tango.DeviceProxy(dev_name)
 print("Ping device:", test_device.ping(), "us")
 print("The device state is:", test_device.state())
