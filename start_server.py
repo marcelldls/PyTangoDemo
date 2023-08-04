d4fd3b109c6571eae88b3f294ea652bbdfee56e3
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-04 11:01:36 +0100

    Add file loading

--- start_server.py
+++ start_server.py
@@ -14,6 +14,9 @@ parser.add_argument(
     "--nodb", help="Run device server without a database", action="store_true"
 )
 parser.add_argument(
+    "--file", help="Run device server without a database but with a properties file", action="store_true"
+)
+parser.add_argument(
     "--test", help="Run test device server (no database)", action="store_true"
 )
 args = parser.parse_args()
@@ -27,10 +30,10 @@ with open("config/config.json", "r", encoding="utf-8") as config_file:
     dev_name = config["device_name"]
     cl_path = config["device_class_path"]
     cl_type = config["device_class_type"]
+    pty_file = config["properties_file"]
 
 srvr_instance = [dsr_name[dsr_name.rfind("/")+1:]]
 srvr_addr = ["--host", host, "--port", port]
-
 
 if args.nodb:
     print("Starting device:",
@@ -45,6 +48,23 @@ if args.nodb:
         dev_config['device_type'] = class_name
         device_class = device_class_builder(**dev_config)
         device_class.run_server(srvr_instance + optn + srvr_addr)
+
+    else:
+        cmnd = ["python", cl_path]
+        os.system(" ".join(cmnd + srvr_instance + optn + srvr_addr))
+
+elif args.file:
+    print("Starting device:",
+          "tango://" + host + ":" + port + "/" + dsr_name + "/" + dev_name)
+    optn = ["--file", pty_file]
+
+    if cl_type == "BuildClass":
+        file_loc = cl_path[:cl_path.rfind(".")].replace("/", ".")
+        dev_config = getattr(importlib.import_module(file_loc), "dev_config")
+        class_name = cl_path[cl_path.find("/") + 1: cl_path.rfind(".")]
+        dev_config['device_type'] = class_name
+        device_class = device_class_builder(**dev_config)
+        device_class.run_server(srvr_instance + srvr_addr + optn)
 
     else:
         cmnd = ["python", cl_path]
