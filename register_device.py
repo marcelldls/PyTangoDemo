7881ced9454a9c0e06ae275437933f6fc1d371c1
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-23 10:23:21 +0100

    Remove redundant line

--- register_device.py
+++ register_device.py
@@ -8,7 +8,6 @@ import tango
 # Process config file
 with open("config/config.json", "r", encoding="utf-8") as config_file:
     config = json.load(config_file)
-    cl_path = config["device_class_path"]
     dsr_name = config["device_server_name"]
     dev_name = config["device_name"]
 
