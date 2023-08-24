c2e6a949b878f052ae2aab3d9b9e63c0181d8889
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-24 16:17:56 +0100

    Add register class/dev properties

--- register_device.py
+++ register_device.py
@@ -17,6 +17,8 @@ with open(args.deviceConfig, "r", encoding="utf-8") as config_file:
     cl_path = config["device_class_path"]
     dsr_name = config["device_server_name"]
     dev_name = config["device_name"]
+    dev_ptys = config["device_properties"]
+    cls_ptys = config["class_properties"]
 
 dev_info = tango.DbDevInfo()
 dev_info.server = dsr_name  # Device server instance name (Device factory)
@@ -25,5 +27,12 @@ dev_info.name = dev_name  # Device instance name
 
 db = tango.Database()
 db.add_device(dev_info)
+db.put_device_property(dev_info.name, dev_ptys)
+db.put_class_property(dev_info._class, cls_ptys)
+
+read = db.get_device_info(dev_info.name)
 
 print("Registered Device:", dev_info.name, "- On device server:", dev_info.server)
+print(read)
+print("Device properties", db.get_device_property_list(dev_info.name, '*'))
+print("Class properties", db.get_class_property_list(dev_info._class))
