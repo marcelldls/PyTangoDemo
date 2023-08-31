f16e259fdd5152ffe533d793fc72c058cb574f11
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-08-31 13:04:41 +0100

    Simplify temp controller implementation

--- tango_classes/PytangoTempController.py
+++ tango_classes/PytangoTempController.py
@@ -7,18 +7,19 @@ import socket
 
 
 class PytangoTempController(Device):
-
     def init_device(self):
         Device.init_device(self)
-        self._suffix = ''.join(re.findall("[\d][\d]\Z", self.get_name()))
+        self._suffix = "".join(re.findall("[\d][\d]\Z", self.get_name()))
         self.set_state(DevState.ON)
         self.sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sckt.connect(( self.host, self.port))
+        self.sckt.connect((self.host, self.port))
 
+    # Properties
 
     host = device_property(dtype=str)
     port = device_property(dtype=int)
 
+    # Attributes
 
     suffix = attribute(dtype=str)
 
@@ -26,83 +27,69 @@ class PytangoTempController(Device):
     def suffix(self):
         return self._suffix
 
-
     enabled = attribute(
-                    dtype=bool, 
-                    polling_period = 200,
-                    )
+        dtype=bool,
+        polling_period=0,
+    )
 
     @enabled.read
     def enabled(self):
-
-        msg = f"N{self._suffix}?\r\n"
-        self.sckt.send(msg.encode("utf-8"))
-        msg_read = self.sckt.recv(1024)  # Keeping it simple
-        self._enabled = bool(int(msg_read.decode("utf-8")))
-
+        self._enabled = bool(int(self.sckt_get("N")))
         return self._enabled
-    
+
     @enabled.write
     def enabled(self, new_enabled):
-        msg = f"N{self._suffix}={int(new_enabled)}\r\n"
-        self.sckt.send(msg.encode("utf-8"))
-
+        self.sckt_put("N", int(new_enabled))
 
     start = attribute(
-                    dtype=int, 
-                    polling_period = 200,
-                    )
+        dtype=int,
+        polling_period=0,
+    )
+
     @start.read
     def enstartd(self):
-
-        msg = f"S{self._suffix}?\r\n"
-        self.sckt.send(msg.encode("utf-8"))
-        msg_read = self.sckt.recv(1024)  # Keeping it simple
-        self._start = int(msg_read.decode("utf-8"))
-
+        self._start = int(self.sckt_get("S"))
         return self._start
-    
+
     @start.write
     def start(self, new_start):
-        msg = f"S{self._suffix}={new_start}\r\n"
-        self.sckt.send(msg.encode("utf-8"))
-
+        self.sckt_put("S", new_start)
 
     current = attribute(
-                    dtype=float, 
-                    polling_period = 0,
-                    )
-    
+        dtype=float,
+        polling_period=0,
+    )
+
     @current.read
     def current(self):
-
-        msg = f"T{self._suffix}?\r\n"
-        self.sckt.send(msg.encode("utf-8"))
-        msg_read = self.sckt.recv(1024)  # Keeping it simple
-        self._start = float(msg_read.decode("utf-8"))
-
-        return self._start
-
+        self._current = float(self.sckt_get("T"))
+        return self._current
 
     end = attribute(
-                dtype=int, 
-                polling_period = 0,
-                )
-    
+        dtype=int,
+        polling_period=0,
+    )
+
     @end.read
     def end(self):
-
-        msg = f"E{self._suffix}?\r\n"
-        self.sckt.send(msg.encode("utf-8"))
-        msg_read = self.sckt.recv(1024)  # Keeping it simple
-        self._end = int(msg_read.decode("utf-8"))
-
+        self._end = int(self.sckt_get("E"))
         return self._end
-    
+
     @end.write
     def end(self, new_end):
-        msg = f"E{self._suffix}={new_end}\r\n"
+        self.sckt_put("E", new_end)
+
+    # Helpers
+    
+    def sckt_put(self, prefix, msg):
+        msg = f"{prefix}{self._suffix}={msg}\r\n"
         self.sckt.send(msg.encode("utf-8"))
+
+    def sckt_get(self, prefix):
+        msg = f"{prefix}{self._suffix}?\r\n"
+        self.sckt.send(msg.encode("utf-8"))
+        msg_read = self.sckt.recv(1024)  # Keeping it simple
+        return msg_read.decode("utf-8")
 
 
 if __name__ == "__main__":
